813402cb573fe7f8aa5363ca1da1d149b8830887
Carlos Egea <32842185+cegea@users.noreply.github.com>
2025-02-22 23:29:07 +0100

    Add LSP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,7 +16,6 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-local opts = {}
 
 require("lazy").setup("plugins")
 
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return{
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
+            lspconfig.clangd.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
