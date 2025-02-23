cbc31ecf0897757699d561f8b8cf5402d021fb0c
Carlos Egea <32842185+cegea@users.noreply.github.com>
2025-02-23 22:47:31 +0100

    Add pyright as lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return{
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "clangd"}
+                ensure_installed = {"lua_ls", "clangd", "pyright"}
             })
         end
     },
@@ -19,6 +19,7 @@ return{
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.clangd.setup({})
+            lspconfig.pyright.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
