9a3cc5b53f669c85f230b806ef94094938aff6b7
Carlos Egea <32842185+cegea@users.noreply.github.com>
2025-02-23 23:02:38 +0100

    Show hidden files by default with neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,15 @@ return {
             "MunifTanjim/nui.nvim"
         },
         config = function()
+            require('neo-tree').setup {
+                filesystem = {
+                    filtered_items = {
+                        visible = true,
+                        hide_dotfiles = false,
+                        hide_gitignored = true,
+                    }
+                }
+            }
             vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
             vim.keymap.set('n', '<C-m>', ':Neotree close<CR>', {})
         end
