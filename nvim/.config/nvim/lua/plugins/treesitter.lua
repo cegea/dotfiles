72dd3c4f8db90bcd312b90b087c45519fc735023
Carlos Egea <32842185+cegea@users.noreply.github.com>
2025-02-24 13:31:40 +0100

    Add python to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter", 
+        "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
             local config = require("nvim-treesitter.configs")
             config.setup({
-                ensure_installed = {"lua", "c"},
+                ensure_installed = {"lua", "c", "python"},
                 highlight = {enable = true},
                 indent = { enable = true },
             })
