b63c815d8676562bff5cdc13ed390e8b6f50e3fa
Carlos Egea <32842185+cegea@users.noreply.github.com>
2025-03-01 14:36:14 +0100

    Enable pyright for x86

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,8 +1,38 @@
 -- Get OS and architecture
 gon = require("os-name")
 os_name, arch_name = gon.getOS()
+--
+
+lsp_confiugration = {}
+
 if arch_name == "x86" then
-    return {}
+    return{
+        {
+            "williamboman/mason.nvim",
+            config = function()
+                require("mason").setup()
+            end
+        },
+        {
+            "williamboman/mason-lspconfig.nvim",
+            config = function()
+                require("mason-lspconfig").setup({
+                    ensure_installed = {"pyright"}
+                })
+            end
+        },
+        {
+            "neovim/nvim-lspconfig",
+            config = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.pyright.setup({})
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+                vim.keymap.set('n', 'K', vim.lsp.buf.code_action, {})
+            end
+        }
+    }
 else
     return{
         {
